1561ff42d7a3bc6eb2fb464df6a327e57a312f87
雜村　吉晃 <>
2018-08-03 12:17:53 +0900

    bolt responsive １回目のレビュー

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 var menu = document.getElementsByClassName('menu')[0];
 var contactBtn = document.getElementsByClassName('header__right')[0];
 
-menu.onclick = function(e) {
-  console.log(e);
-  console.log('test');
-  console.log(contactBtn.style.display);
+if (window.innerWidth <= 766) {
+  contactBtn.style.display = 'none'
+}
 
+menu.addEventListener('click', function(e) {
+  // console.log(e);
+  // console.log('test');
   if (contactBtn.style.display === 'none') {
     contactBtn.style.display = 'block';
   } else {
     contactBtn.style.display = 'none';
   }
-}
+});
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 766) {
+    contactBtn.style.display = 'none'
+  } else {
+    contactBtn.style.display = 'block'
+  }
+}, false);
